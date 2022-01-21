3f94c4b7aea51fe68e49d47a30cdc78b37bb80bb
Myuze <Myuze@users.noreply.github.com>
2022-01-21 01:09:32 -0800

    Commented out unused auth server code

--- client/src/utils/auth.tsx
+++ client/src/utils/auth.tsx
@@ -1,28 +1,28 @@
 import decode from 'jwt-decode';
 
 class AuthService {
-  getUser() {
-    return decode(this.getToken());
-  }
+  // getUser(): string | null {
+  //   return decode(this.getToken());
+  // }
 
-  loggedIn() {
-    // Checks if there is a saved token and it's still valid
-    const token = this.getToken();
-    return !!token && !this.isTokenExpired(token);
-  }
+  // loggedIn() {
+  //   // Checks if there is a saved token and it's still valid
+  //   const token = this.getToken();
+  //   return !!token && !this.isTokenExpired(token);
+  // }
 
-  isTokenExpired(token: string) {
-    try {
-      const decoded = decode(token);
-      if (decoded.exp < Date.now() / 1000) {
-        return true;
-      } else return false;
-    } catch (err) {
-      return false;
-    }
-  }
+  // isTokenExpired(token: string) {
+  //   try {
+  //     const decoded = decode(token);
+  //     if (decoded.exp < Date.now() / 1000) {
+  //       return true;
+  //     } else return false;
+  //   } catch (err) {
+  //     return false;
+  //   }
+  // }
 
-  getToken() {
+  getToken(): string | null {
     // Retrieves the user token from localStorage
     return localStorage.getItem('id_token');
   }
